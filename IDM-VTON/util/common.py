882ee57a03eaddf1c37963f887a04b0b3c110272
MoJieAIGC <cynicbreakout@gmail.com>
2024-07-04 16:31:41 +0800

    install first

--- IDM-VTON/util/common.py
+++
@@ -1,8 +0,0 @@
-import platform, os
-
-def open_folder():
-    open_folder_path = os.path.abspath("outputs")
-    if platform.system() == "Windows":
-        os.startfile(open_folder_path)
-    elif platform.system() == "Linux":
-        os.system(f'xdg-open "{open_folder_path}"')
